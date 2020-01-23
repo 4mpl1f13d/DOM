fa985c3e0d84cabc2f6d432802bdb4b0d9e42c0c
4mpl1f13d <4mpl1f13d@users.noreply.github.com>
2020-01-23 10:16:02 -0600

    Add files via upload

---
+++ DOM/scripts.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let paragraph = document.createElement('p');
+    let text = document.createTextNode('This can be whatever text you would like it to be!');
+
+    paragraph.style.color = "red";
+    paragraph.style.textTransform = "uppercase";
+    paragraph.className = "some-paragraph";
+
+    paragraph.appendChild(text);
+    document.body.appendChild(paragraph);
+
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('Clicky Me!');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+
+    button.addEventListener("click", function () {
+        let h1 = document.createElement('h1');
+        let h1Text = document.createTextNode("I've been clicked...");
+        h1.appendChild(h1Text);
+        document.body.appendChild(h1);
+    })
+
+})
+
+
+
